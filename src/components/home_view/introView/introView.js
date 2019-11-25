3c66fc207c6f668595bd8d04d7cc995cdd935e6f
IMCBER <imcber1@gmail.com>
2019-11-24 23:44:37 -0600

    working with the menu

--- src/components/home_view/introView/introView.js
+++ src/components/home_view/introView/introView.js
@@ -1,24 +1,36 @@
 import React,{useEffect,useState} from 'react';
 import './introView.css';
 
+//this the intro view, contains the principal menu and paralllax img.
 function IntroView(props){
     return(
         <section className={'section-container'}>
             <div className={'intro-container'}>
-
+                <MenuList />
             </div>
         </section>
     );
 }
 
 function MenuList(props){
-    const listItemMenu = [{}];
+    const listItemMenu = [
+        {title:'About',section:'about'},
+        {title:'Tech',section:'about'},
+        {title:'Work',section:'about'},
+        {title:'Contanct',section:'about'}
+    ];
+
+    return(
+        <div>
+            {listItemMenu.map((item) => <MenuItem key={item.section} item={item}/>)}
+        </div>
+    );
 }
 
 function MenuItem(props){
     return(
         <>
-            <span className={'menu-item'}>{props.item.title}</span>
+            <a className={'menu-item'}>{props.item.title}</a>
         </>
     );
 }
