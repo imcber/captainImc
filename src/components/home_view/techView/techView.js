232c1ef54da2daea98f7822fe4569c17d1e0bcf3
imcber <imcber1@gmail.com>
2019-12-03 18:55:37 -0600

    init view Tech

--- src/components/home_view/techView/techView.js
+++ src/components/home_view/techView/techView.js
@@ -7,8 +7,28 @@ const TechView = () =>{
     const getDataLeg = useStateData();
     return(
         <SectionContainer id={'techView'} title={getDataLeg('lg.menu.tech.title')}>
-            <h1>TECH VIEW</h1>
+            <TechContent />
         </SectionContainer>
+    );
+}
+
+const TechContent = () => {
+    const TechList = () =>{
+        const list = [
+            {name:'JavaScript',icon:''},
+            {name:'ReactJS',icon:''},
+            {name:'Angular',icon:''}
+        ];
+        return(
+            <div>
+                {list.map((item) => item.name)}
+            </div>
+        );
+    };
+    return(
+        <div className={"rest-container-body"}>
+            <TechList />
+        </div>
     );
 }
 
