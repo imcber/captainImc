92583afc966fb1e633f9c5d1f4fb628b42f16f9d
imcber <imcber1@gmail.com>
2019-12-11 14:30:44 -0600

    working with list of sites

--- src/components/home_view/workView/workView.js
+++ src/components/home_view/workView/workView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './workView.css';
 import {SectionContainer} from '../homeView';
 import {useStateData} from '../../utils/state';
@@ -7,60 +7,91 @@ import {useStateData} from '../../utils/state';
 const WorkView = () =>{
     //GET DATA JSON
     const getDataLeg = useStateData();
+    //GET LIST OF MY WORK
+    const listJsonSites = getDataLeg('lg.menu.work.sites');
+    //OBJECT OF ACTUAL ACTUAL SITE TO DISPLAY
+    const [actualWorkDisp,setActualWorkDisp] = useState(listJsonSites[0]);
+    
     return(
         <SectionContainer id={'workView'} title={getDataLeg('lg.menu.work.title')} style={{display:'flex'}} classHeader={''}>
-            <WorkContent />
+            <WorkContent actualWorkDisp={actualWorkDisp}/>
+            <ListSitesSection listJsonSites={listJsonSites}/>
         </SectionContainer>
     );
 };
 
-//CONTENT OF ALL THE VIEW
-const WorkContent = () =>{
-    const getDataLeg = useStateData();
-    const listJsonSites = getDataLeg('lg.menu.work.sites');
-    return(
-        <>
-            <BigViewSite />
-            <ListSitesSection listJsonSites={listJsonSites}/>
-        </>
-    );
-};
-
+//SECTION OF WORK LIST
 const ListSitesSection = props => {
+    //LIST OF ALL SITES
     const listJsonSites = props.listJsonSites;
-    return(
-        <div className={'list-site-section'}>
-            {listJsonSites.map(item => item.name)}
-        </div>
-    );
-};
+    let listPagination = [];
+    let listAuxTemp = [];
+    listJsonSites.map((item,indx) => {
+        if(indx < props.lengthDisplay){
+            listAuxTemp.push(item);
+        }else{
+            listPagination.push(listAuxTemp);
+            listAuxTemp = [];
+            
+        }
+    });
 
-//THIS SHOW DE ACTUAL IMG AND INFO ABOUT SELECTED SITE
-const BigViewSite = () =>{
-    const ContainerInView = props =>{
+    //CARD OF EACH SITE
+    const CardSite = props => {
+        const thisItem = props.item;
+        const heightDina = (100/3)+'%';
         return(
-            <div style={{width:'100%',height:props.height}}>
-                {props.children}
+            <div className={'card-site'} style={{height:heightDina}}>
+                <span>{thisItem.name}</span>
             </div>
         );
     };
 
     return(
-        <div className={'big-view-site'}>
-            <ContainerInView height={'15%'}>
-                <span style={{float:"left"}}>NAME SITE</span>
-                <span style={{float:"right"}}>GO</span>
-            </ContainerInView>
-            <ContainerInView height={'65%'}>
-                <span style={{float:"left"}}>NAME SITE</span>
-            </ContainerInView>
-            <ContainerInView height={'20%'}>
-                <span style={{float:"left"}}>NAME SITE</span>
-            </ContainerInView>
+        <div className={'list-site-section'}>
+            {listJsonSites.map(item => <CardSite item={item}/>)}
         </div>
     );
-}
+};
 
+//CONTENT OF ALL THE VIEW
+const WorkContent = props =>{
+    const actualWorkDisp = props.actualWorkDisp;
+
+    //THIS SHOW DE ACTUAL IMG AND INFO ABOUT SELECTED SITE
+    const BigViewSite = () =>{
+        //Work that display in the view
+        const actualSite = actualWorkDisp;
+        const ContainerInView = props =>{
+            return(
+                <div style={{width:'100%',height:props.height}}>
+                    {props.children}
+                </div>
+            );
+        };
+
+        return(
+            <div className={'big-view-site'}>
+                <ContainerInView height={'15%'}>
+                    <span style={{float:"left"}}>{actualSite.name}</span>
+                    <span style={{float:"right"}}>GO</span>
+                </ContainerInView>
+                <ContainerInView height={'65%'}>
+                    <span style={{float:"left"}}>NAME SITE</span>
+                </ContainerInView>
+                <ContainerInView height={'20%'}>
+                    <span style={{float:"left"}}>{actualSite.desc}</span>
+                </ContainerInView>
+            </div>
+        );
+    }
+
+    return(
+        <>
+            <BigViewSite actualSite={actualWorkDisp}/>
+        </>
+    );
+};
 
 
 export default WorkView;
