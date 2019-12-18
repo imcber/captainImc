09280c6b3c60b7756a904b438a7cf4c894fc524a
imcber <imcber1@gmail.com>
2019-12-17 18:24:04 -0600

    adding some funtions

--- src/components/home_view/workView/workView.js
+++ src/components/home_view/workView/workView.js
@@ -3,34 +3,52 @@ import './workView.css';
 import {SectionContainer} from '../homeView';
 import {useStateData} from '../../utils/state';
 
+const beraviImg = require('../../../assets/img/sites/Beravi.jpg');
+const leafireImg = require('../../../assets/img/sites/Beravi.jpg');
+const cthcImg = require('../../../assets/img/sites/Beravi.jpg');
+const listImgSites = {
+    beravi:beraviImg,
+    leafire:leafireImg,
+    cthc:cthcImg
+};
+
 //INIT WORK VIEW
 const WorkView = () =>{
     //GET DATA JSON
     const getDataLeg = useStateData();
+    //NUM OF CARDS TO DISPLAY
+    const numListDisplay = 2;
     //GET LIST OF MY WORK
     const listJsonSites = getDataLeg('lg.menu.work.sites');
+    //GET LIST OF LIST TO DISPLAY
+    const listPagination = getListPagination(listJsonSites,numListDisplay);
     //OBJECT OF ACTUAL ACTUAL SITE TO DISPLAY
-    const [actualWorkDisp,setActualWorkDisp] = useState(listJsonSites.beravi);
+    const [actualWorkDisp,setActualWorkDisp] = useState(listPagination[0][0]);
 
     const handlerClick = item =>{
-        setActualWorkDisp(item)
+        setActualWorkDisp(item);
     };
     
     return(
         <SectionContainer id={'workView'} title={getDataLeg('lg.menu.work.title')} style={{display:'flex'}} classHeader={'header-work-section'}>
             <WorkContent actualWorkDisp={actualWorkDisp}/>
-            <ListSitesSection listJsonSites={listJsonSites} numList={2} handlerClick={handlerClick}/>
+            <ListSitesSection listPagination={listPagination} numList={numListDisplay} handlerClick={handlerClick}/>
         </SectionContainer>
     );
 };
 export default WorkView;
+
 //SECTION OF WORK LIST
 const ListSitesSection = props => {
-    //LIST OF ALL SITES
-    const listJsonSites = props.listJsonSites;
+    //NUMBER OF IMGS TO DISPLAY
     const numListDisplay = props.numList;
-    let listPagination = getListPagination(listJsonSites,numListDisplay);
-    const [listDisplay,setListPagination] = useState(listPagination[0]);
+    //GET LIST OF LIST TO DISPLAY
+    let listPagination = props.listPagination;
+    //HOOK OF NUMBER
+    const [numList,setNumList] = useState(0);
+    //ACTUAL LIST TO DISPLAY
+    const listDisplay = listPagination[numList];
+    //EVENT TO CHANGE SITE
     const onHandlerClick = props.handlerClick;
     //CARD OF EACH SITE
     const CardSite = props => {
@@ -44,22 +62,40 @@ const ListSitesSection = props => {
         return(
             <div className={'card-site'} style={{height:heightDina}} onClick={clickFunction}>
                 <span>{thisItem.name}</span>
+                <img src={thisItem.img} alt={thisItem.name} className={'img-card'}></img>
             </div>
         );
+    };
+
+    //NAVIGATION BUTOON TO LIST SITES
+    const NavigationButton = props =>{
+        const navDirection = props.direction === "prev"?'<':'>';
+        const clickNavigate = () =>{
+            navigatePagination(props.direction);
+        };
+        return(
+            <div className={'navigation-list-button'} onClick={clickNavigate}>
+                <span>{navDirection}</span>
+            </div>
+        );
+    };
+    //FUNCTION TO NAV IN THE PAGINATION OF LIST SITES
+    const navigatePagination = position =>{
+        //GET NEXT OR BACK
+        let newNumPag = position === "next"?numList+1:numList-1;
+        //IF NEW NUM IS > OF LENGTH LIST PAGINATION, RETURN THE FIRST. IF NEW NUM IS LESS THAN 0, SO SET THE LAST OF LIST PAGINATION
+        newNumPag = newNumPag >= listPagination.length?0:newNumPag < 0?listPagination.length - 1:newNumPag;
+        setNumList(newNumPag);
     };
 
     return(
         <div style={{width:'35%'}}>
             <div className={'list-site-section'}>
-                <div className={'navigation-list-button'}>
-                    <span>{'<'}</span>
-                </div>
+                {listPagination.length > 1 && <NavigationButton direction={'prev'}/>}
                 <div style={{width:'80%'}}>
                     {listDisplay.map(item => <CardSite item={item} key={item.name.toUpperCase()}/>)}
                 </div>
-                <div className={'navigation-list-button'}>
-                    <span>{'>'}</span>
-                </div>
+                {listPagination.length > 1 && <NavigationButton direction={'next'}/>}
             </div>
             <div className={'navigation-list-sites'}>
                 
@@ -91,7 +127,7 @@ const WorkContent = props =>{
                     <span style={{float:"right"}}>GO</span>
                 </ContainerInView>
                 <ContainerInView height={'65%'}>
-                    <span style={{float:"left"}}>NAME SITE</span>
+                    <img src={actualSite.img} alt={actualSite.name} className="img-display"></img>
                 </ContainerInView>
                 <ContainerInView height={'20%'}>
                     <span style={{float:"left"}}>{actualSite.desc}</span>
@@ -118,7 +154,7 @@ const getListPagination = (listOriginal,numInd) => {
             listAuxTemp = new Array();
             indx = 1;
         }
-        
+        listOriginal[item].img = listImgSites[item.toLowerCase()];
         listAuxTemp.push(listOriginal[item]);
         indx++;
     });
